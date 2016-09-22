9579b62ea45089bf56615c71b698d10c182b588b
James Maynard <jameskirkmaynard@gmail.com>
2016-09-22 08:47:56 -0700

    Add add year form on book show view, define new and create in years controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   end
   def show
     @book = Book.find(params[:id])
+    @year = Year.new
   end
   def new
     @book = Book.new
--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -7,4 +7,24 @@ class YearsController < ApplicationController
     @year = Year.find(params[:id])
     @book = Book.new
   end
+  def new
+    @year = Year.new
+    @books = Book.all
+  end
+  def create
+    @year = Year.new(year_params)
+    @book = Book.find(@year.book_ids)
+      if @year.save
+        redirect_to book_path(@book)
+      else
+        render :new
+      end
+  end
+
+  private
+    def year_params
+      params.require(:year).permit(:year, :book_ids)
+    end
+
+
 end
