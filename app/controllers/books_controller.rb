18944d0a9fecdb9f11f0ede7ea440824a85eaca4
James Maynard <jameskirkmaynard@gmail.com>
2016-09-24 14:20:05 -0700

    Update book_params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,6 +36,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :author, :year_ids)
+    params.require(:book).permit(:title, :author_first_name, :author_last_name, :year_ids)
   end
 end
